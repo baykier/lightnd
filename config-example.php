1eb186be548dcca9a3175a5733654048d059c0db
baykier <aigoudebook@gmail.com>
2016-11-03 20:36:21 +0800

    config example added

---
+++ config-example.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * Author: Baykier<1035666345@qq.com>
+ * Date: 16-11-3
+ * Time: 下午8:13
+ */
+
+$config = array();
+
+$config['db'] = array(
+    'default' => array(
+        'driver' => 'mysql',
+        'host' => 'your database host',
+        'dbname' => 'your dbname',
+        'user' => 'your name',
+        'password' => 'your password',
+        'charset' => 'UTF-8',
+    ),
+);
