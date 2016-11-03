0d1c64298dbc63e96e0a9cf2ba0adbbc674e619a
baykier <aigoudebook@gmail.com>
2016-11-03 10:08:07 +0000

    test command basic complete

--- src/TestCommand.php
+++ src/TestCommand.php
@@ -13,6 +13,9 @@ use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Input\InputDefinition;
 use Symfony\Component\Console\Helper\QuestionHelper;
+use Symfony\Component\Console\Helper\Table;
+use Symfony\Component\Console\Helper\ProgressBar;
+use Symfony\Component\Console\Question\Question;
 
 class TestCommand extends BaseCommand
 {
@@ -27,19 +30,58 @@ class TestCommand extends BaseCommand
             ->setDescription('测试打字速度')
             ->addArgument('number',InputArgument::OPTIONAL,'每次打字显示的数量',10)
             ->addArgument('level',InputArgument::OPTIONAL,'级别设置',1)
-            ->addOption('log-time','-lt',InputOption::VALUE_OPTIONAL,'是否记录打字时间',null);
+            ->addOption('no-show-time','-t',InputOption::VALUE_NONE,'是否记录打字时间',null);
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         //是否记录测试时间
-        $logTime = $input->getOption('log-time');
+        $logTime = $input->getOption('no-show-time');
         $logTime = !$logTime ? true : false;
-
-        $testStart = time();
-
         $words = $this->getWords();
+        if ($words)
+        {
+            //显示前10个 查询次数最多的单词
+            $table = new Table($output);
+            $table->setHeaders(array('单词','释义','查询次数'));
+            foreach ($words as $word)
+            {
+                $table->addRow(array($word['word'],$word['desc'],$word['query_count']));
+            }
+            $table->render();
+            //开始记录打字
+            $testStart = time();
+            $successCount = 0 ;
+            $space = 10;
+            $quest = new QuestionHelper();
+            $processBar = new ProgressBar($output,self::DEFAULT_WORD_NUMBER * $space);
+            $output->writeln("开始测试..\n");
+            $processBar->setOverwrite(true);
+            $processBar->display();
+            $output->writeln("\n");
+            foreach ($words as $word)
+            {
+                $typeWord = '';
+                while ($typeWord != $word['word'])
+                {
+                    $typeWord = $quest->ask($input,$output,new Question(sprintf("%s:\n",$word['word']),''));
+                }
+                $successCount++;
+                $processBar->advance($space);
+                $output->writeln("\n");
+            }
+            $processBar->finish();
+            $output->writeln('\n测试完成');
+            if ($logTime)
+            {
+                $output->writeln(sprintf("测试共用时:%s 秒",time() - $testStart));
+            }
 
+        }
+        else
+        {
+            $output->writeln("您的词库还未存储任何单词，请先添加 [lightnd add]");
+        }
 
     }
 }
